69d94457c3e1372c58e9416469744c58923ffa01
Matthew <mxmdmiller@gmail.com>
2020-09-25 01:42:11 -0400

    Initial commit

---
+++ src/test/java/com/search/groupme/SpringApplicationTest.java
@@ -0,0 +1,13 @@
+package com.search.groupme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
