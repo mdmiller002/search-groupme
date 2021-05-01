26d04672ac39f1c084371e631e7df95f915ff384
Matthew Miller <mxmdmiller@gmail.com>
2021-05-01 16:36:30 -0400

    Implement message update algorithm initialization
    Implement the algorithm to reach out to GroupMe,
    fetch latest messages, and index them into
    Elasticsearch to be searchable. Implements the initialization
    phase of the algorithm, where we simply do the first pass
    of indexing messages from groups.

--- src/main/java/com/search/jsonModels/Group.java
+++ src/main/java/com/search/jsonModels/Group.java
@@ -10,6 +10,13 @@ public class Group {
   private long id;
   private String name;
 
+  public Group() { };
+
+  public Group(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
   public String getName() {
     return name;
   }
--- src/main/java/com/search/rdbms/hibernate/models/GroupEntity.java
+++ src/main/java/com/search/rdbms/hibernate/models/GroupEntity.java
@@ -3,6 +3,7 @@ package com.search.rdbms.hibernate.models;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = GroupEntity.TABLE_NAME)
@@ -11,29 +12,66 @@ public class GroupEntity {
   public static final String TABLE_NAME = "GROUPS";
 
   @Id
-  long id;
-  long mostRecentMessageId;
+  Long id;
+  Long topPointer;
+  Long bottomPointer;
+
+  boolean isInitialized;
 
   public GroupEntity() { }
 
-  public GroupEntity(long id, long mostRecentMessageId) {
+  public GroupEntity(Long id, Long topPointer, Long bottomPointer, boolean isInitialized) {
     this.id = id;
-    this.mostRecentMessageId = mostRecentMessageId;
+    this.topPointer = topPointer;
+    this.bottomPointer = bottomPointer;
+    this.isInitialized = isInitialized;
   }
 
-  public long getId() {
+  public Long getId() {
     return id;
   }
 
-  public void setId(long id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
-  public long getMostRecentMessageId() {
-    return mostRecentMessageId;
+  public Long getTopPointer() {
+    return topPointer;
   }
 
-  public void setMostRecentMessageId(long mostRecentMessageId) {
-    this.mostRecentMessageId = mostRecentMessageId;
+  public void setTopPointer(Long topPointer) {
+    this.topPointer = topPointer;
+  }
+
+  public Long getBottomPointer() {
+    return bottomPointer;
+  }
+
+  public void setBottomPointer(Long bottomPointer) {
+    this.bottomPointer = bottomPointer;
+  }
+
+  public boolean getInitialized() {
+    return isInitialized;
+  }
+
+  public void setInitialized(boolean isInitialized) {
+    this.isInitialized = isInitialized;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupEntity that = (GroupEntity) o;
+    return id.equals(that.id) &&
+        Objects.equals(topPointer, that.topPointer) &&
+        Objects.equals(bottomPointer, that.bottomPointer) &&
+        Objects.equals(isInitialized, that.isInitialized);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, topPointer, bottomPointer, isInitialized);
   }
 }
